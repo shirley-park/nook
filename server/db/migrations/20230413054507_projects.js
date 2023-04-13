c7f2c474d934e526b9a5e2ca71223d174fdb3c9e
Shirley Park <shirley.park7@gmail.com>
2023-04-13 18:03:44 +1200

    migration for projects db

---
+++ server/db/migrations/20230413054507_projects.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('projects', (table) => {
+    table.increments('id')
+    table.string('space')
+    table.longtext('description')
+    table.string('image_gallery')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('projects')
+}
