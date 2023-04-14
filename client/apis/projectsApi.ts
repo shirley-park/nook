017c999976aed61631a775ea7eeff8b1198709b5
Shirley Park <shirley.park7@gmail.com>
2023-04-14 15:08:56 +1200

    add api function to fetch all elements and projects

---
+++ client/apis/projectsApi.ts
@@ -0,0 +1,13 @@
+import request from 'superagent'
+
+import projectModel from '../models/projectModel'
+
+// --------------------
+
+const url = '/api/v1/nook/'
+
+export function fetchProjectsApi(): Promise<projectModel[]> {
+  return request.get(url).then((res) => {
+    return res.body
+  })
+}
