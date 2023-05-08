badf810338f6fb2711453871579356949ddd9e43
Shirley Park <shirley.park7@gmail.com>
2023-05-09 10:41:39 +1200

    refactor reducers

--- client/reducers/index.ts
+++ client/reducers/index.ts
@@ -4,8 +4,6 @@ import { combineReducers } from 'redux'
 import projectsReducer from './projectsReducer'
 import elementsReducer from './elementsReducer'
 
-// import stuff from './stuff'
-
 export default combineReducers({
   projectsState: projectsReducer,
   elementsState: elementsReducer,
