3dfc77ea5bfc9bea46857014e3f1f12f09b31e40
Shirley Park <shirley.park7@gmail.com>
2023-05-08 16:38:29 +1200

    remove test components

--- client/components/FadeInAnimation.tsx
+++ client/components/FadeInAnimation.tsx
@@ -11,24 +11,9 @@ const FadeInAnimation = ({
 }) => {
   const Component = wrapperElement
   let compRef = useRef(null)
-  // const distance = 200
+
   const fadeDirection = { x: 0 }
-  // switch (direction) {
-  // case 'left':
-  //   fadeDirection = { x: -distance }
-  //   break
-  // case 'right':
-  //   fadeDirection = { x: distance }
-  //   break
-  // case 'up':
-  //   fadeDirection = { y: distance }
-  //   break
-  // case 'down':
-  //   fadeDirection = { y: -distance }
-  //   break
-  // default:
-  //   fadeDirection = { x: 0 }
-  // }
+
   useEffect(() => {
     gsap.from(compRef.current, 1, {
       ...fadeDirection,
